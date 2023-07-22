7f81f9c51d95e9c55f6bc0f4d80365808f9896c9
Thiago Pereira <72038599+Thiago2VP@users.noreply.github.com>
2023-07-22 02:19:32 -0300

    true inital commit

---
+++ theme.bash
@@ -0,0 +1,87 @@
+
+# Colors
+background='#24273a'
+foreground='#cad3f5'
+color0='#181926'
+color1='#f5bde6'
+color2='#a6da95'
+color3='#f5a97f'
+color4='#8aadf4'
+color5='#c6a0f6'
+color6='#7dc4e4'
+color7='#939ab7'
+color8='#5b6078'
+color9='#ee99a0'
+color10='#8bd5ca'
+color11='#eed49f'
+color12='#91d7e3'
+color13='#b7bdf8'
+color14='#7dc4e4'
+color15='#8087a2'
+
+accent='#b7bdf8'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='JetBrains Mono:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Arc-Circle'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='arc.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Catppuccin-Macchiato-Standard-Lavender-Dark'
+icon_theme='Arc-Circle'
+cursor_theme='Qogirr'
+
+# Openbox
+ob_theme='Catppuccin-Macchiato'
+ob_layout='DLIMC'
+ob_font='JetBrains Mono'
+ob_font_size='9'
+ob_menu='menu-icons.xml'
+ob_margin_t='48'
+ob_margin_b='10'
+ob_margin_l='10'
+ob_margin_r='10'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x48'
+dunst_origin='top-right'
+dunst_font='JetBrains Mono 10'
+dunst_border='4'
+dunst_separator='2'
+
+# Plank
+plank_hmode='intelligent'
+plank_offset='0'
+plank_position='bottom'
+plank_theme='Transparent'
+plank_icon_size='32'
+plank_zoom_percent='120'
+
+# Picom
+picom_backend='glx'
+picom_corner='6'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
